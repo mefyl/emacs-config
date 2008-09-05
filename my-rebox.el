362bbf63c3dfc64d56fda2d8075b14b12ad8edb9
mefyl <hocquet@gostai.com>
2008-09-05 12:00:07 +0200

    Cleanup my-rebox.el.

--- my-rebox.el
+++ my-rebox.el
@@ -3,17 +3,11 @@
 (defvar my-rebox-style 523)
 
 (make-variable-buffer-local 'my-rebox-style)
-;  523 lisp
 
 (defun my-rebox-comment (style)
   (interactive "P")
   (if style
       (let ((rebox-default-style my-rebox-style)) (rebox-comment nil))
       (rebox-comment nil)))
-
-;; (defun setup-c-rebox ()
-;;   (local-set-key "\M-q" 'my-rebox-comment))
-
-;; (add-hook 'c++-mode-hook 'setup-c-rebox)
 
 (provide 'my-rebox)
