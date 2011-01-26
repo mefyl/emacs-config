e8ba0a593e0b2a36490c8524333a72cc2643ef72
mefyl <mefyl@gruntech.org>
2011-01-26 02:02:23 +0100

    Emacs: do not load all files upon startup, rely on the tags.

--- my-c-project.el
+++ my-c-project.el
@@ -1,5 +1,5 @@
 ;;;###autoload
-(defun c++-project (sources incpath buildpath buildcmd binary args)
+(defun c++-project (incpath buildpath buildcmd binary args)
   (let ((root (file-name-directory load-file-name)))
     ;; Helper
     ;; FIXME: make that local
@@ -18,5 +18,19 @@
     (setq my-gdb-args args)
 
     ;; Open files
-    (mapcar (lambda (p) (find-file (path p))) sources)
+    ;; (mapcar (lambda (p) (find-file (path p))) sources)
+
+    ;; Load tags
+    (visit-tags-table (concat root "/TAGS"))
+
+    ;; Override find-file
+    (global-set-key [(control x) (control f)] 'c++-project-find-file)
     ))
+
+(defun c++-project-find-file (&optional any)
+  (interactive "P")
+  (if any
+      (ido-find-file)
+    (save-excursion
+      (visit-tags-table-buffer)
+      (find-file (ido-completing-read "File: " (tags-table-files) () t "" () ())))))
