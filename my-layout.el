53eb8bbcf0e3f2720db577e53ab29734d94ae56e
mefyl <mefyl@gruntech.org>
2011-02-19 00:01:51 +0100

    Emacs: add dedicated windows shortcut.

--- my-layout.el
+++ my-layout.el
@@ -3,3 +3,15 @@
   (interactive "P")
   (let ((width (or width 80)))
     (shrink-window (- (window-width) width) t)))
+
+;;;###autoload
+(defun toggle-current-window-dedication ()
+ (interactive)
+ (let* ((window    (selected-window))
+        (dedicated (window-dedicated-p window)))
+   (set-window-dedicated-p window (not dedicated))
+   (message "Window %sdedicated to %s"
+            (if dedicated "no longer " "")
+            (buffer-name))))
+
+(global-set-key [pause] 'toggle-current-window-dedication)
