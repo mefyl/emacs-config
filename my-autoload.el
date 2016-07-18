5ababd44f3d2a3e55784857b672420bbc74a9f1d
mefyl <mefyl@gruntech.org>
2016-07-18 15:37:31 +0200

    Update autoloads.

--- my-autoload.el
+++ my-autoload.el
@@ -1,7 +1,7 @@
 ;;;***
 
-;;;### (autoloads (elle-log-mode) "elle-log-mode" "elle-log-mode.el"
-;;;;;;  (21383 20960 659094 81000))
+;;;### (autoloads nil "elle-log-mode" "elle-log-mode.el" (22406 3853
+;;;;;;  784010 817000))
 ;;; Generated autoloads from elle-log-mode.el
 
 (autoload 'elle-log-mode "elle-log-mode" "\
@@ -11,8 +11,8 @@ Major mode for viewing elle log files
 
 ;;;***
 
-;;;### (autoloads (turn-on-fci-mode fci-mode) "fill-column-indicator"
-;;;;;;  "fill-column-indicator.el" (21074 47689 877686 308000))
+;;;### (autoloads nil "fill-column-indicator" "fill-column-indicator.el"
+;;;;;;  (22406 3853 784010 817000))
 ;;; Generated autoloads from fill-column-indicator.el
 
 (autoload 'fci-mode "fill-column-indicator" "\
@@ -38,8 +38,7 @@ Turn on fci-mode unconditionally.
 
 ;;;***
 
-;;;### (autoloads (flx-ido-mode) "flx-ido" "flx-ido.el" (21316 7916
-;;;;;;  614078 46000))
+;;;### (autoloads nil "flx-ido" "flx-ido.el" (22406 3853 785010 817000))
 ;;; Generated autoloads from flx-ido.el
 
 (defvar flx-ido-mode nil "\
@@ -55,9 +54,8 @@ Toggle flx ido mode
 
 ;;;***
 
-;;;### (autoloads (framemove-default-keybindings fm-right-frame fm-left-frame
-;;;;;;  fm-up-frame fm-down-frame) "framemove" "framemove.el" (21074
-;;;;;;  47689 877686 308000))
+;;;### (autoloads nil "framemove" "framemove.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from framemove.el
 
 (autoload 'fm-down-frame "framemove" "\
@@ -89,8 +87,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (c++-project) "my-c-project" "my-c-project.el"
-;;;;;;  (21316 6771 363042 961000))
+;;;### (autoloads nil "my-c-project" "my-c-project.el" (22406 3853
+;;;;;;  785010 817000))
 ;;; Generated autoloads from my-c-project.el
 
 (autoload 'c++-project "my-c-project" "\
@@ -100,8 +98,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads nil "my-compile" "my-compile.el" (22029 25835 745972
-;;;;;;  634000))
+;;;### (autoloads nil "my-compile" "my-compile.el" (22412 56196 51
+;;;;;;  20000))
 ;;; Generated autoloads from my-compile.el
 
 (autoload 'my-recompile "my-compile" "\
@@ -111,8 +109,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (insert-fixme) "my-fixme" "my-fixme.el" (21074
-;;;;;;  47689 877686 308000))
+;;;### (autoloads nil "my-fixme" "my-fixme.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from my-fixme.el
 
 (autoload 'insert-fixme "my-fixme" "\
@@ -122,8 +120,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (toggle-current-window-dedication window-set-width)
-;;;;;;  "my-layout" "my-layout.el" (21074 47689 877686 308000))
+;;;### (autoloads nil "my-layout" "my-layout.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from my-layout.el
 
 (autoload 'window-set-width "my-layout" "\
@@ -138,8 +136,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (my-log-view-mode-setup) "my-log-view-mode" "my-log-view-mode.el"
-;;;;;;  (21383 19807 266046 730000))
+;;;### (autoloads nil "my-log-view-mode" "my-log-view-mode.el" (22406
+;;;;;;  3853 785010 817000))
 ;;; Generated autoloads from my-log-view-mode.el
 
 (autoload 'my-log-view-mode-setup "my-log-view-mode" "\
@@ -149,8 +147,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (python-mode-setup) "my-python-mode" "my-python-mode.el"
-;;;;;;  (21528 15884 330420 137000))
+;;;### (autoloads nil "my-python-mode" "my-python-mode.el" (22406
+;;;;;;  3853 785010 817000))
 ;;; Generated autoloads from my-python-mode.el
 
 (autoload 'python-mode-setup "my-python-mode" "\
@@ -160,8 +158,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (my-rebox-comment) "my-rebox" "my-rebox.el" (21074
-;;;;;;  47689 877686 308000))
+;;;### (autoloads nil "my-rebox" "my-rebox.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from my-rebox.el
 
 (autoload 'my-rebox-comment "my-rebox" "\
@@ -171,8 +169,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (my-rm-set-mark) "my-rect-mark" "my-rect-mark.el"
-;;;;;;  (21342 37622 273480 162000))
+;;;### (autoloads nil "my-rect-mark" "my-rect-mark.el" (22406 3853
+;;;;;;  785010 817000))
 ;;; Generated autoloads from my-rect-mark.el
 
 (autoload 'my-rm-set-mark "my-rect-mark" "\
@@ -182,8 +180,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads (sh-mode-setup) "my-sh-mode" "my-sh-mode.el" (21074
-;;;;;;  47689 877686 308000))
+;;;### (autoloads nil "my-sh-mode" "my-sh-mode.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from my-sh-mode.el
 
 (autoload 'sh-mode-setup "my-sh-mode" "\
@@ -193,8 +191,8 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
-;;;### (autoloads nil "qml-mode" "qml-mode.el" (22172 45172 284049
-;;;;;;  258000))
+;;;### (autoloads nil "qml-mode" "qml-mode.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from qml-mode.el
 
 (autoload 'qml-mode "qml-mode" "\
@@ -204,9 +202,8 @@ Major mode for Qt declarative UI
 
 ;;;***
 
-;;;### (autoloads (rm-mouse-drag-region rm-kill-ring-save rm-kill-region
-;;;;;;  rm-exchange-point-and-mark rm-set-mark rm-example-picture-mode-bindings)
-;;;;;;  "rect-mark" "rect-mark.el" (21341 40732 809917 384000))
+;;;### (autoloads nil "rect-mark" "rect-mark.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from rect-mark.el
  (define-key ctl-x-map "r\C-@" 'rm-set-mark)
  (define-key ctl-x-map [?r ?\C-\ ] 'rm-set-mark)
@@ -266,270 +263,8 @@ This must be bound to a button-down mouse event.
 
 ;;;***
 
-;;;### (autoloads (rtags-diagnostics rtags-clear-diagnostics rtags-stop-diagnostics
-;;;;;;  rtags-post-command-hook rtags-restart-tracking-timer rtags-update-current-project
-;;;;;;  rtags-fix-fixit-at-point rtags-cycle-overlays-on-screen rtags-is-running
-;;;;;;  rtags-clear-diagnostics-overlays rtags-apply-fixit-at-point
-;;;;;;  rtags-find-references-current-dir rtags-find-symbol-current-dir
-;;;;;;  rtags-find-references-current-file rtags-find-symbol-current-file
-;;;;;;  rtags-find-references rtags-find-symbol rtags-rename-symbol
-;;;;;;  rtags-guess-function-at-point rtags-find-all-references-at-point
-;;;;;;  rtags-find-virtuals-at-point rtags-find-references-at-point
-;;;;;;  rtags-find-symbol-at-point rtags-location-stack-reset rtags-location-stack-back
-;;;;;;  rtags-location-stack-forward rtags-quit-rdm rtags-print-current-location
-;;;;;;  rtags-enable-standard-keybindings rtags-location-stack-jump
-;;;;;;  rtags-goto-offset rtags-print-enum-value-at-point rtags-print-dependencies
-;;;;;;  rtags-print-cursorinfo rtags-set-current-project rtags-maybe-reparse-file
-;;;;;;  rtags-reparse-file rtags-preprocess-file rtags-index-js-file
-;;;;;;  rtags-previous-diag rtags-next-diag rtags-previous-match
-;;;;;;  rtags-next-match rtags-bury-or-delete) "rtags" "rtags.el"
-;;;;;;  (21559 55610 501013 180000))
-;;; Generated autoloads from rtags.el
-
-(autoload 'rtags-bury-or-delete "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-next-match "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-previous-match "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-next-diag "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-previous-diag "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-index-js-file "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-preprocess-file "rtags" "\
-
-
-\(fn &optional BUFFER)" t nil)
-
-(autoload 'rtags-reparse-file "rtags" "\
-WAIT-REPARSING : t to wait for reparsing to finish, nil for async (no waiting).
-:fixme: add a timeout
-
-\(fn &optional BUFFER WAIT-REPARSING)" t nil)
-
-(autoload 'rtags-maybe-reparse-file "rtags" "\
-
-
-\(fn &optional BUFFER)" t nil)
-
-(autoload 'rtags-set-current-project "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-print-cursorinfo "rtags" "\
-
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-print-dependencies "rtags" "\
-
-
-\(fn &optional BUFFER)" t nil)
-
-(autoload 'rtags-print-enum-value-at-point "rtags" "\
-
-
-\(fn &optional LOCATION)" t nil)
-
-(autoload 'rtags-goto-offset "rtags" "\
-
-
-\(fn POS)" t nil)
-
-(autoload 'rtags-location-stack-jump "rtags" "\
-
-
-\(fn BY)" t nil)
-
-(autoload 'rtags-enable-standard-keybindings "rtags" "\
-
-
-\(fn &optional MAP PREFIX)" t nil)
-
-(autoload 'rtags-print-current-location "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-quit-rdm "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-location-stack-forward "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-location-stack-back "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-location-stack-reset "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-find-symbol-at-point "rtags" "\
-Find the natural target for the symbol under the cursor and moves to that location.
-For references this means to jump to the definition/declaration of the referenced symbol (it jumps to the definition if it is indexed).
-For definitions it jumps to the declaration (if there is only one) For declarations it jumps to the definition.
-If called with a prefix restrict to current buffer
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-find-references-at-point "rtags" "\
-Find all references to the symbol under the cursor
-If there's exactly one result jump directly to it.
-If there's more show a buffer with the different alternatives and jump to the first one if rtags-jump-to-first-match is true.
-References to references will be treated as references to the referenced symbol
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-find-virtuals-at-point "rtags" "\
-List all reimplentations of function under cursor. This includes both declarations and definitions
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-find-all-references-at-point "rtags" "\
-
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-guess-function-at-point "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-rename-symbol "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-find-symbol "rtags" "\
-
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-find-references "rtags" "\
-
-
-\(fn &optional PREFIX)" t nil)
-
-(autoload 'rtags-find-symbol-current-file "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-find-references-current-file "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-find-symbol-current-dir "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-find-references-current-dir "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-apply-fixit-at-point "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-clear-diagnostics-overlays "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-is-running "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-cycle-overlays-on-screen "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-fix-fixit-at-point "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-update-current-project "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-restart-tracking-timer "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-post-command-hook "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-stop-diagnostics "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-clear-diagnostics "rtags" "\
-
-
-\(fn)" t nil)
-
-(autoload 'rtags-diagnostics "rtags" "\
-
-
-\(fn &optional RESTART NODIRTY)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../dev/mmm-mode/mmm-auto.el" "../dev/mmm-mode/mmm-class.el"
-;;;;;;  "../dev/mmm-mode/mmm-cmds.el" "../dev/mmm-mode/mmm-compat.el"
-;;;;;;  "../dev/mmm-mode/mmm-cweb.el" "../dev/mmm-mode/mmm-defaults.el"
-;;;;;;  "../dev/mmm-mode/mmm-mason.el" "../dev/mmm-mode/mmm-mode.el"
-;;;;;;  "../dev/mmm-mode/mmm-myghty.el" "../dev/mmm-mode/mmm-noweb.el"
-;;;;;;  "../dev/mmm-mode/mmm-region.el" "../dev/mmm-mode/mmm-rpm.el"
-;;;;;;  "../dev/mmm-mode/mmm-sample.el" "../dev/mmm-mode/mmm-univ.el"
-;;;;;;  "../dev/mmm-mode/mmm-utils.el" "flx.el" "my-elisp.el" "my-font.el"
-;;;;;;  "my-lisp-mode.el" "my-shebang.el" "my-templates.el" "rebox.el"
-;;;;;;  "sgml.el" "tuareg.el") (22333 40235 151087 868000))
-
-;;;***
-
-;;;### (autoloads nil "my-c-mode" "my-c-mode.el" (22333 39737 574853
-;;;;;;  708000))
+;;;### (autoloads nil "my-c-mode" "my-c-mode.el" (22406 3853 785010
+;;;;;;  817000))
 ;;; Generated autoloads from my-c-mode.el
 
 (autoload 'sandbox "my-c-mode" "\
