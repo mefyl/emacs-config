c08e242edb37ed611640e1969e68558629eb070a
mefyl <mefyl@gruntech.org>
2017-12-19 12:57:36 +0100

    Improve go mode.

--- my-go-mode.el
+++ my-go-mode.el
@@ -1,4 +1,11 @@
+(require 'go-mode)
+(add-hook 'before-save-hook 'gofmt-before-save)
+(define-key go-mode-map [(control c) (control c)] 'comment-region)
+(define-key go-mode-map [(meta .)] 'godef-jump)
+
+(custom-set-variables
+ '(gofmt-args (quote ("-s"))))
+
 ;;;###autoload
 (defun go-mode-setup ()
-  (require 'go-mode-autoloads)
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (require 'go-mode-autoloads))
